156004706d9d36c9e70b21aba81af3276faee539
Andreii <andreyhodirev@gmail.com>
2018-11-17 15:39:33 +0200

    make seeder - type_connections

--- resources/views/abons/_new.blade.php
+++ resources/views/abons/_new.blade.php
@@ -18,7 +18,7 @@
                             @endforeach
                         </select>
                     </div>
-                    <div class="form-group">
+                    <div class="form-group" id="type-ch-form">
                         {{Form::label('t_connection', 'Тип подключения')}}
                         <select name="t_connections" id="">
                             @foreach($t_connections as $tcon)
@@ -48,8 +48,50 @@
                     </div>
                     <div class="form-group">
                         {{Form::label('comment', 'Смета')}}
-                        {{Form::textarea('comment','',['id'=> 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Смета по подключению'])}}
+                        {{Form::textarea('comment','',['id'=> 'article-ckeditor', 'class' => 'form-control', 'placeholder' => '100м - 600грн + подключени 600грн'])}}
                     </div>
+                    <div class="form-group">
+                        {{Form::label('all_money', 'Полная стоимость')}}
+                        {{Form::textarea('all_money','',['id'=> 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'ТУТ ПРОСТО ЧИСЛО СУММА'])}}
+                    </div>
+                    {{-- Если выбран тип подключения GPON то показывать этот див  --}}
+                    <div hidden id="pon_info">
+                        <div class="form-group">
+                            {{Form::label('leng', 'Длинна кабеля')}}
+                            {{Form::textarea('leng','',['id'=> 'article-ckeditor', 'class' => 'form-control', 'placeholder' => '150метров'])}}
+                        </div>
+                        <div class="form-group">
+                            {{Form::label('point_inc', 'Точка включения')}}
+                            {{Form::textarea('point_inc','',['id'=> 'article-ckeditor', 'class' => 'form-control', 'placeholder' => '25Т 3М'])}}
+                        </div>
+                        <div class="form-group">
+                            {{Form::label('mac_onu', 'MAC ONU')}}
+                            {{Form::textarea('mac_onu','',['id'=> 'article-ckeditor', 'class' => 'form-control', 'placeholder' => '1с23.2224.dbc2'])}}
+                        </div>
+                    </div>
+                    {{-- Если выбран тип подключения Wifi то показывать этот див  --}}
+                    <div hidden id="wifi_info">
+                        <div class="form-group">
+                            {{Form::label('leng', 'Длинна кабеля')}}
+                            {{Form::textarea('leng','',['id'=> 'article-ckeditor', 'class' => 'form-control', 'placeholder' => '150метров'])}}
+                        </div>
+                        <div class="form-group">
+                            {{Form::label('base_ip', 'Базовая станция')}}
+                            {{Form::textarea('base_ip','',['id'=> 'article-ckeditor', 'class' => 'form-control', 'placeholder' => '46.164.132.206:53302'])}}
+                        </div>
+                        <div class="form-group">
+                                {{Form::label('client_ip', 'Клиентская антена')}}
+                                {{Form::textarea('client_ip','',['id'=> 'article-ckeditor', 'class' => 'form-control', 'placeholder' => '46.164.132.206:53310'])}}
+                        </div>
+                    </div>  
+                    {{-- Если выбран тип подключения МЕДЬ то показывать этот див  --}}
+                    <div hidden id="cable_info">
+                        <div class="form-group">
+                            {{Form::label('leng', 'Длинна кабеля')}}
+                            {{Form::textarea('leng','',['id'=> 'article-ckeditor', 'class' => 'form-control', 'placeholder' => '150метров'])}}
+                        </div>
+                    </div>
+                    
             </div>      
             <!-- Modal footer -->
             <div class="modal-footer">
@@ -60,3 +102,14 @@
         </div>
     </div>
 </div>
+
+<script>
+      $('.type-ch-form').change(function() {
+                    var id = this.value;
+                    var city_id = $(".city-ch option:selected").val();
+                    var url = '/datatablesFindTConIDBase/'+id+'/'+city_id;
+                    var table = $('#abon_table').DataTable();
+                    $('#main_table').css('display','block');
+
+                });
+</script>
