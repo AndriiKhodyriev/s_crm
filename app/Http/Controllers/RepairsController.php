f3a112b49c78a7dc560494d8e5d87d251f5ad20f
Kronos sys <andreyhodirev@gmail.com>
2019-02-13 14:29:06 +0200

    f

--- app/Http/Controllers/RepairsController.php
+++ app/Http/Controllers/RepairsController.php
@@ -30,7 +30,7 @@ class RepairsController extends Controller
                 array_push($cities_id, $city->id);
             };
         //$cities = City::all();
-            $cities = City::select(['id','name'])
+            $cities = City::select(['id','name', 'visibility_everywhere'])
                     ->whereIn('id', $cities_id)
                     ->get();
         } else {
